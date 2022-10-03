d9a8998c6e42566e42cc280b2b72ef772d52b96c
Vivian Chen <vivian.chen1193@gmail.com>
2022-10-03 13:33:30 -0700

    finished creating tables for department, role, and employee in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -16,4 +16,20 @@ CREATE TABLE role (
     FOREIGN KEY department_id
     REFERENCES department(id)
     ON DELETE SET NULL
-)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    
+    role_id INT NOT NULL,
+    FOREIGN KEY role_id
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+
+    manager_id INT NOT NULL,
+    FOREIGN KEY manager_id
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
