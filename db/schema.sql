6d478f451206d6562d7f16269c4477e64c6512a0
Vivian Chen <vivian.chen1193@gmail.com>
2022-10-03 13:27:21 -0700

    created tables in schema for department and role

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,17 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY department_id
+    REFERENCES department(id)
+    ON DELETE SET NULL
+)
--- server.js
+++ server.js
@@ -1,3 +1,28 @@
+// Importing
 const cTable = require('console.table');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employee_db'
+});
+
+const userChoices = () => {
+    inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'userChoices',
+            message: 'Which of the following would you like to do?',
+            choices: ['View all departments', 
+            'View all roles', 
+            'View all employees', 
+            'Add a department', 
+            'Add a role', 
+            'Add an employee', 
+            'Update an employee role']
+        }
+    ])
+}
